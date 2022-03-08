a8ce488c1ecf102e25a1483800b90c1511c86977
driftcar10 <lev.Potoskuev@gmail.com>
2022-03-08 13:52:48 +0000

    swiched to docker compose

---
+++ python/scripts/app.py
@@ -0,0 +1,10 @@
+print("Hello from Container!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=5000)
