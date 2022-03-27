34f7c5b4fa510f0ca57dcc494803c5e627c49723
driftcar10 <lev.Potoskuev@gmail.com>
2022-03-27 15:43:48 +0000

    rendered first template (login)

--- python/scripts/app.py
+++ python/scripts/app.py
@@ -1,10 +1,20 @@
 print("Hello from Container!")
-from flask import Flask
-
+from flask import Flask, redirect, render_template, request, session
+from helpers import login_required
 app = Flask(__name__)
 
 @app.route('/')
+@login_required
 def index():
     return 'Web App with Python Flask! And hello from Lev! From container!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+#    session.clear()
+
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('login.html')
 
 app.run(host='0.0.0.0', port=5000)
