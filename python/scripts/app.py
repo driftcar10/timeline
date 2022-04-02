37990587d46c2ce7615e16f912805b5054b4b5d3
driftcar10 <lev.Potoskuev@gmail.com>
2022-04-02 16:36:52 +0000

    Hooked mariadb in app and created styles.css

--- python/scripts/app.py
+++ python/scripts/app.py
@@ -1,7 +1,23 @@
 print("Hello from Container!")
 from flask import Flask, redirect, render_template, request, session
 from helpers import login_required
+import mariadb
+import os
 app = Flask(__name__)
+
+user = os.environ.get('MYSQL_USER')
+password = os.environ.get('MYSQL_PASSWORD')
+database = os.environ.get('MYSQL_DATABASE')
+
+conn = mariadb.connect(
+        host = "db",
+        port = 3306,
+        user = user,
+        password = password,
+        database = database
+    )
+
+cur = conn.cursor()
 
 @app.route('/')
 @login_required
@@ -13,6 +29,9 @@ def login():
 #    session.clear()
 
     if request.method == 'POST':
+        cur.execute("SELECT * FROM users")
+        for user in cur:
+            print(user)
         return render_template('indev.html')
     else:
         return render_template('login.html')
