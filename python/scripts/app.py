fe4a23abbe1647f49687781169ada5cf7a901e64
driftcar10 <lev.Potoskuev@gmail.com>
2022-04-03 12:12:35 +0000

    nearly done login and added apology

--- python/scripts/app.py
+++ python/scripts/app.py
@@ -1,6 +1,7 @@
 print("Hello from Container!")
 from flask import Flask, redirect, render_template, request, session
-from helpers import login_required
+from helpers import login_required, apology
+from werkzeug.security import check_password_hash, generate_password_hash
 import mariadb
 import os
 app = Flask(__name__)
@@ -29,10 +30,30 @@ def login():
 #    session.clear()
 
     if request.method == 'POST':
-        cur.execute("SELECT * FROM users")
-        for user in cur:
-            print(user)
-        return render_template('indev.html')
+        
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        
+        cur.execute("SELECT password_hash, id FROM users WHERE name = ?", (request.form.get("username"), ))
+        password, user_id = cur.fetchone()
+        
+        print(password)
+        print(user_id)
+
+        if not password:
+            return apology("invalid username and/or password", 403)
+
+        if not check_password_hash(password, request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+        
+        session["user_id"] = user_id
+
+        return redirect("/")
+        #return render_template('indev.html')
     else:
         return render_template('login.html')
 
--- python/scripts/helpers.py
+++ python/scripts/helpers.py
@@ -1,12 +1,16 @@
-from flask import redirect, session
+from flask import redirect, session, render_template
 from functools import wraps
 
-def login_required(f):
-    """
-    Decorate routes to require login.
+def apology(message, code=400):
+    def escape(s):
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
 
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
+def login_required(f):
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
